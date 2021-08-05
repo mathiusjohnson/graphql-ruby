509c26224481ea37c9e9d2b21cbfd26adff169c1
Mathius <devachievem@gmail.com>
2021-08-05 09:15:39 -0700

    add validation to link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ApplicationRecord
-  belongs_to :user, validate: true
+  belongs_to :user, optional: true
 
-  has_many :votes, dependent: :destroy
+  validates :url, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 5 }
+
+  has_many :votes
 end
