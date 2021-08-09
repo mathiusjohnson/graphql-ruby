e71846ca415c07bc8b394ece4e9203c468e3af75
Mathius <devachievem@gmail.com>
2021-08-09 13:51:31 -0700

    add extra stylesheets to manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .css
+
+//= link_directory ../../javascript/stylesheets .css
 //= link graphiql/rails/application.css
 //= link graphiql/rails/application.js
