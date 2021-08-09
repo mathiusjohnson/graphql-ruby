27ad15f91eaf2f84a04891a479aa2610ff3c9cd2
Mathius <devachievem@gmail.com>
2021-08-09 13:53:48 -0700

    add graphlient connection to BE in links controller

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,35 @@
+class LinksController < ApplicationController
+  Client = Graphlient::Client.new('http://localhost:3000/graphql',
+    # headers: {
+    #   'Authorization' => 'Bearer 123'
+    # },
+    http_options: {
+      read_timeout: 20,
+      write_timeout: 30
+    }
+  )
+
+  Response = Client.query <<~GRAPHQL
+    query {
+      allLinks {
+        id
+        description
+        postedBy {
+          name
+        }
+        votes {
+          user {
+            email
+          }
+        }
+      }
+    }
+  GRAPHQL
+
+
+  def index
+    print Response.data
+    @data = Response.data.all_links
+  end
+
+end
